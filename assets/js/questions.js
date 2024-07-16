239ea9d77e8cb816b0c901ea9b47a90e46fd6a22
Henna Singh <hennasingh04@gmail.com>
2024-07-16 18:07:40 +0000

    game page: Add structure and styles for game page. Moved javascript from index page to index html to keep separate concerns

---
+++ assets/js/questions.js
@@ -0,0 +1,26 @@
+export const questions = [ 
+    {
+        id:1,
+        option:"The Dark Arts"
+    },
+    {
+        id:2,
+        option:"Magical Objects"
+    },
+    {
+        id:3,
+        option:"Hogwarts"
+    },
+    {
+        id:4,
+        option:"Magical People"
+    },
+    {
+        id:5,
+        option:"Magical Spells & Potions"
+    },
+    {
+        id:6,
+        option:"Animals & Magical Creatures"
+    }
+]
