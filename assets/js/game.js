b9b402d7a4a410a5d2e5d32ed666762e16c4b1fe
Henna Singh <hennasingh04@gmail.com>
2024-07-18 07:19:20 +0000

    game page: Add quiz categories and game controls structure and styles

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
 import { questions } from "./questions.js";
+
+//Wait for the DOM to finish loading before running the game
+//Get the button elements and add event listeners to them
+
+const categButtons = document.getElementsByClassName('categ-btn')
+
+for( let category of categButtons) {
+    category.addEventListener('click', function() {
+        let quizType = this.getAttribute('data-type')
+        runQuiz(quizType)
+    })
+}
+
+function runQuiz(quizType) {
+    
+}
