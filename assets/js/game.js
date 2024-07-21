562cc86bd11ac11eee179940141325d239d86d8f
Henna Singh <hennasingh04@gmail.com>
2024-07-21 18:23:33 +0000

    game page: Add questions in dark arts category and logic to display ques on screen

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,13 @@
 import { questions } from "./questions.js";
+const questionTitle = document.getElementById('ques-title')
+const submitBtn = document.getElementById('submit')
+const inputAnswer = document.getElementById('answer')
+
+//variables to hold values
+
+let availableQuestions = []
+let currentQuestion = []
+let magicalItems = []
 
 //Wait for the DOM to finish loading before running the game
 //Get the button elements and add event listeners to them
@@ -8,10 +17,33 @@ const categButtons = document.getElementsByClassName('categ-btn')
 for( let category of categButtons) {
     category.addEventListener('click', function() {
         let quizType = this.getAttribute('data-type')
+        document.getElementById('categories-container').style.display = "none";
         runQuiz(quizType)
     })
 }
-
+/**
+ * 
+ * The function loads the questions from the selected category
+ * and unhide answer input and submit options
+ */
 function runQuiz(quizType) {
+    const quiz = questions.filter((item) => item.id == quizType)[0]
+    availableQuestions = quiz.questions 
+    document.getElementById('input-container').classList.remove('hide');
+    submitBtn.classList.remove('hide')
     
+    displayQuestion()
+}
+
+function displayQuestion() {
+    let random = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions.splice(random,1)[0]
+    console.log(currentQuestion.text) 
+    questionTitle.textContent = currentQuestion.text
+    submitBtn.addEventListener('click', checkAnswer)
+}
+
+function checkAnswer() {
+    const userAnswer = inputAnswer.value
+    console.log(userAnswer)
 }
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,7 +1,33 @@
 export const questions = [ 
     {
         id:1,
-        option:"The Dark Arts"
+        option:"The Dark Arts",
+        questions: [
+            {
+                text: "Who did R.A.B. instruct to destroy the locket Horcrux after his death?",
+                correctAnswers: ["kreacher"]
+            },
+            {
+                text:"Who kills Nagini?",
+                correctAnswers:["Neville Longbottom", "neville"]
+            },
+            {
+                text:"What name does Harry give when caught by the Snatchers?",
+                correctAnswers:["Vernon Dudley", "vernon"]
+            },
+            {
+                text:"Who is taken captive when Harry goes to face Voldemort in the Forbidden Forest after the battle of Hogwarts?",
+                correctAnswers:["hagrid"]
+            },
+            {
+                text:"According to Professor Slughorn, what is the purpose of a Horcrux? a)Immortality, b)Power, c)Victory?",
+                correctAnswers:["a)Immortality", "immortality"]
+            },
+            {
+                text:"Who destroys the locket Horcrux?",
+                correctAnswers:["Ron Weasley", "Ron", "ron"]
+            }
+        ]
     },
     {
         id:2,
