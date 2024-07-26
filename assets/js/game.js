7fa34425702bd31b4348e4154cc0e2b9a975e6e4
Henna Singh <hennasingh04@gmail.com>
2024-07-26 15:50:35 +0000

    Game Page: Add description to game functions and set timout to display color for correct/wrong answers. Set color styles

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,6 +3,7 @@ const questionTitle = document.getElementById('ques-title')
 const submitBtn = document.getElementById('submit')
 const inputAnswer = document.getElementById('answer')
 const magicSpellEarned = document.getElementById('magic-spells')
+const gameContainer = document.getElementById('game-container')
 
 //variables to hold values
 
@@ -26,38 +27,79 @@ for( let category of categButtons) {
 /**
  * 
  * The function loads the questions from the selected category
- * and unhide answer input and submit options
+ * and unhide answer input and submit options.
+ * Add click listener to submit button
+ * and calls displayQuestion function
+ * Code help taken from https://www.youtube.com/watch?v=riDzcEQbX6k
  */
 function runQuiz(quizType) {
     const quiz = questions.filter((item) => item.id == quizType)[0]
     availableQuestions = quiz.questions 
+
     document.getElementById('input-container').classList.remove('hide');
     submitBtn.classList.remove('hide');
+
     document.getElementById('scoreboard').classList.remove('hide');
     submitBtn.addEventListener('click', checkAnswer);
+
     displayQuestion();
 }
 
+/**
+ * The function selects a random question from the list and displays it on the screen for the user.
+ * The question is removed from the list so its not selected again.
+ * Code reference taken from https://github.com/Ant2210/project2/blob/main/assets/js/quiz.js
+ * 
+ */
 function displayQuestion() {
     let random = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions.splice(random,1)[0]
-    console.log(currentQuestion.text) 
+
     questionTitle.textContent = currentQuestion.text
     
 }
 
+/**
+ * This function is called when user clicks on submit button 
+ * the function compares user answer to current question answer and add
+ * a green color to the game container for correct answer and red for incorrect.
+ * 
+ * The user is awarded a random spell from the list for correct answer.
+ * The function checks the count of remaining questions and calls end.html if no ques is left
+ */
+
 function checkAnswer() {
     const userAnswer = inputAnswer.value
     if(currentQuestion.correctAnswers.includes(userAnswer.trim().toLowerCase())) {
-        let random = Math.floor(Math.random() * magicalSpells.length);
-        awardedSpell.push(magicalSpells.splice(random,1)[0])
-        magicSpellEarned.textContent = [...awardedSpell].join(', ')
+        gameContainer.classList.add('correct');
 
-    } 
-    inputAnswer.value = ''
-    if (availableQuestions.length === 0) {
-        localStorage.setItem('magicalItems', JSON.stringify(awardedSpell))
-        return window.location.assign("end.html")
+        setTimeout(() => {
+            let random = Math.floor(Math.random() * magicalSpells.length);
+            awardedSpell.push(magicalSpells.splice(random,1)[0]);
+
+            magicSpellEarned.textContent = [...awardedSpell].join(', ');
+            gameContainer.classList.remove("correct");
+
+            if (availableQuestions.length === 0) {
+                localStorage.setItem('magicalItems', JSON.stringify(awardedSpell))
+                return window.location.assign("end.html")
+            }
+            inputAnswer.value = ''
+            displayQuestion()
+        }, 1000);
+
+    } else {
+        gameContainer.classList.add('wrong');
+
+        setTimeout(() => {
+            gameContainer.classList.remove("wrong");
+
+            if (availableQuestions.length === 0) {
+                localStorage.setItem('magicalItems', JSON.stringify(awardedSpell))
+                return window.location.assign("end.html")
+            }
+            inputAnswer.value = ''
+            displayQuestion()
+        }, 1000);
     }
-    displayQuestion()
 }
