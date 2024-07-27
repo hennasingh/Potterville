8f6298df5579d3ad44fcc3c0b6be636807ecbac0
Henna Singh <hennasingh04@gmail.com>
2024-07-27 18:54:59 +0000

    website: Add font-awesome kit to all html pages and icons to end page. Adjusted audio margin and rename score to IQ

--- assets/js/end.js
+++ assets/js/end.js
@@ -5,6 +5,10 @@ const totalScoreSpan = document.getElementById('total-score')
 const spells = JSON.parse(localStorage.getItem('magicalItems'));
 const category = localStorage.getItem('category');
 
+/**
+ * The function is run when the page is loaded.
+ * It displays the score, category and magic quotient on the screen.
+ */
 document.addEventListener('DOMContentLoaded', () =>{
     magicSpellEarned.textContent = [...spells].join(', ')
     categorySpan.textContent = category
