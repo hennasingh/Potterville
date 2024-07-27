82faec38f66732549c1b8cc1e8e531cca5110456
Henna Singh <hennasingh04@gmail.com>
2024-07-27 15:22:06 +0000

    End Page: Add score total feature and margin-top to audio

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,5 +1,6 @@
 const magicSpellEarned = document.getElementById('magic-spells')
 const categorySpan = document.getElementById('category')
+const totalScoreSpan = document.getElementById('total-score')
 
 const spells = JSON.parse(localStorage.getItem('magicalItems'));
 const category = localStorage.getItem('category');
@@ -7,4 +8,7 @@ const category = localStorage.getItem('category');
 document.addEventListener('DOMContentLoaded', () =>{
     magicSpellEarned.textContent = [...spells].join(', ')
     categorySpan.textContent = category
+    totalScoreSpan.textContent = spells.length * 10
+    
+    
 })
